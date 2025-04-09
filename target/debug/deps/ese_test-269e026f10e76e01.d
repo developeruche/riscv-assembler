aaaa3c7ff054fd8b3e5c3b2637e24e2faac4e11d
developeruche <developeruche@gmail.com>
2025-04-09 08:34:20 +0100

    crates and bins setup

--- target/debug/deps/ese_test-269e026f10e76e01.d
+++
@@ -1,5 +0,0 @@
-/Users/gregg/Documents/projects/PROJECTS/riscv-assembler/target/debug/deps/libese_test-269e026f10e76e01.rmeta: bins/ese-test/src/main.rs
-
-/Users/gregg/Documents/projects/PROJECTS/riscv-assembler/target/debug/deps/ese_test-269e026f10e76e01.d: bins/ese-test/src/main.rs
-
-bins/ese-test/src/main.rs:
--- target/debug/deps/ese_test-f8fd5b7e4918ac2d.d
+++
@@ -1,5 +0,0 @@
-/Users/gregg/Documents/projects/PROJECTS/riscv-assembler/target/debug/deps/libese_test-f8fd5b7e4918ac2d.rmeta: bins/ese-test/src/main.rs
-
-/Users/gregg/Documents/projects/PROJECTS/riscv-assembler/target/debug/deps/ese_test-f8fd5b7e4918ac2d.d: bins/ese-test/src/main.rs
-
-bins/ese-test/src/main.rs:
